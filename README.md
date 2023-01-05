f5fe3a72ac1602db2fca0c12f442a37fac250142
kei-tenkawa <bz5adgjmptw0@yahoo.co.jp>
2023-01-05 18:48:13 +0900

    [fix] db postgres -> mysql and fix migration files

--- README.md
+++ README.md
@@ -6,11 +6,21 @@
 [Ruby on RailsのAPIのconfig/routes.rb書き方【ルーティング備忘録】]https://usconsort.com/rails-routes/
 [Next.js + Ruby on Rails(APIモード) on Docker 構築手順](https://zenn.dev/taku1115/articles/6c9fa97ab37e38)
 ```
-docker compose run web rails ---
+docker compose run api rails ---
 
 // routes一覧確認
-docker compose run web rails routes
+docker compose run api rails routes
 
 // gemfileを編集後
 docker compose build
+
+// modelを作成+migrationも作成
+docker compose run api rails g model model名
+
+// db作成
+docker compose run api rails db:create
+docker compose run api rails db:migrate
+
+// db削除
+docker compose run api rails db:drop
 ```
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_053737) do
+ActiveRecord::Schema.define(version: 2023_01_05_094340) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_06_20_053737) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -36,13 +33,13 @@ ActiveRecord::Schema.define(version: 2022_06_20_053737) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "departments", force: :cascade do |t|
+  create_table "departments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sections", force: :cascade do |t|
+  create_table "sections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,16 +47,33 @@ ActiveRecord::Schema.define(version: 2022_06_20_053737) do
     t.index ["department_id"], name: "index_sections_on_department_id"
   end
 
-  create_table "task_types", force: :cascade do |t|
+  create_table "task_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "tasks_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tasks_id"], name: "index_task_statuses_on_tasks_id"
+  end
+
+  create_table "task_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "tasks" because of following StandardError
-#   Unknown type 'task_status' for column 'status'
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "task_type_id"
+    t.index ["name"], name: "index_tasks_on_name", unique: true
+    t.index ["task_type_id"], name: "index_tasks_on_task_type_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
