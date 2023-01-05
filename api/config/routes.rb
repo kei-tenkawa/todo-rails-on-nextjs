85deb0764a02e52f9acb886ea32c01e9756f985b
kei-tenkawa <bz5adgjmptw0@yahoo.co.jp>
2023-01-05 13:01:10 +0900

    [add] docker frontend api

---
+++ api/config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  scope :api do
+    scope :v1 do
+      # admin
+      namespace :admin do
+        resources :users do
+          get 'sections', on: :member
+        end
+      end
+      # ログイン
+      resources :login, only: %i[new create]
+
+      # ログアウト
+      resources :logout, only: [:destroy]
+      # TODO
+      root to: 'tasks#index'
+      resources :tasks do
+        post :confirm, action: :confirm_new, on: :new
+        post :import, on: :collection
+      end
+      # task_type
+      resources :task_types
+
+      # 日報作成
+      resources :daily_report, only: %i[index create]
+    end
+  end
+end
