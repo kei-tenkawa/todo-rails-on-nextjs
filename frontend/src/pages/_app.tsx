6aa9aec68aae3fe0415849d4f4445266546e6459
kei-tenkawa <bz5adgjmptw0@yahoo.co.jp>
2023-01-09 22:44:45 +0900

    [add] _app _document

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// カスタムApp
+// https://nextjs-ja-translation-docs.vercel.app/docs/advanced-features/custom-app
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+// カスタムDocument
+// https://nextjs-ja-translation-docs.vercel.app/docs/advanced-features/custom-document
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="研修で作成した純正RailsアプリにNext.jsを組み合わせてみた" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
